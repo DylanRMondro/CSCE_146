333b9b855abc23a73a6e2b11534eadb3bc4bfd1b
dylan <dylan@DESKTOP-78RB533>
2019-02-07 16:00:27 -0500

    added ListNodes, heads, curr, prev, and created the GenLinkedList method

--- Homework_01/src/GenLinkedList.java
+++ Homework_01/src/GenLinkedList.java
@@ -1,4 +1,17 @@
 //Dylan Mondro
-public class GenLinkedList {
- 
+public class GenLinkedList <M>{
+	private class ListNode{
+		private M data;
+		private ListNode link;
+		public ListNode(M aData, ListNode aLink) {
+			data = aData;
+			link = aLink;
+		}
+	}
+	private ListNode head; //points to the first item in list
+	private ListNode curr; //point to the current node of interest
+	private ListNode prev; //One node behind current
+	public GenLinkedList() {
+		head = curr = prev = null;
+	}
 }
