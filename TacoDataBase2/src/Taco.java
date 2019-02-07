d7d44ef89ed20def7c2b25453aa1faea687f21da
dylan <dylan@DESKTOP-78RB533>
2019-02-07 16:37:18 -0500

    getters and setters

--- TacoDataBase2/src/Taco.java
+++ TacoDataBase2/src/Taco.java
@@ -1,4 +1,46 @@
-
+//Notes
 public class Taco {
-
+	private String name, loc;
+	private double price;
+	private int rating;//1-5
+	public Taco() { //default constructor
+		this.name = this.loc = "none";
+		this.price = 0.0;
+		this.rating = 1;
+	}
+	public Taco(String aName, String aLoc, double aPrice, int aRating) {
+		this.setName(aName);
+		this.setLoc(aLoc);
+		this.setPrice(aPrice);
+		this.setRating(aRating);
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLoc() {
+		return loc;
+	}
+	public void setLoc(String loc) {
+		this.loc = loc;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		if(price >= 0.0)
+			this.price = price;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		if(rating >=1 && rating <=5)
+			this.rating = rating;
+		else
+			this.rating = 1;
+	}
+	
 }
