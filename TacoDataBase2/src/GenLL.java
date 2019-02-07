46ba3ee8ed9f06311a3f13368df8eb5fa702eaef
dylan <dylan@DESKTOP-78RB533>
2019-02-07 17:02:06 -0500

    add getCurrent, setCurrent, goToNect, resetCurr

--- TacoDataBase2/src/GenLL.java
+++ TacoDataBase2/src/GenLL.java
@@ -8,4 +8,28 @@ public class GenLL <T>{
 			link = aLink;
 		}
 	}
+	private ListNode head; //points to first item
+	private ListNode curr; // points to item of interest
+	private ListNode prev; //one behind the current
+	public GenLL() {
+		head = curr = prev = null;
+	}
+	public T getCurrent() {
+		if(curr != null)
+			return curr.data;
+		return null;
+	}
+	public void setCurrent(T aData) {//curr will point to the head
+		if(curr != null)
+			curr.data = aData;
+	}
+	public void goToNext() {
+		if(curr == null)
+			return;
+		prev = curr;
+		curr = curr.link;
+	}
+	public void resetCurr() { //points the curr to the head
+		curr = head;
+	}
 }
