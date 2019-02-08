bbc0462285d33a3b50e89be510b8d8a6a0ae1834
BuildTools <unconfigured@null.spigotmc.org>
2019-02-07 22:34:55 -0500

    added toString and equals

--- Homework_01/src/Movie.java
+++ Homework_01/src/Movie.java
@@ -6,7 +6,7 @@ public class Movie {
 		this.name = director = "none";
 		this.year = 1888; //Year of the first movie
 		this.boxOfficeGross = 0;
-		this.rating = 1;
+		this.rating = 1; //1-5
 	}
 	public Movie(String Aname, String aDirector, int aYear, int baBxOfficeGross, int aRating) {
 		
@@ -27,7 +27,7 @@ public class Movie {
 		return year;
 	}
 	public void setYear(int year) {
-		if(year >= 1888)
+		if(year >= 1888) //can't be older than 1888
 			this.year = year;
 		else
 			this.year = 1888; //if later than 1888 then it will set it back to 1888
@@ -36,7 +36,7 @@ public class Movie {
 		return boxOfficeGross;
 	}
 	public void setBoxOfficeGross(int boxOfficeGross) {
-		if(boxOfficeGross > 0)
+		if(boxOfficeGross > 0) //can't be any lower than 0
 			this.boxOfficeGross = boxOfficeGross;
 		else
 			this.boxOfficeGross = 0;
@@ -45,10 +45,24 @@ public class Movie {
 		return rating;
 	}
 	public void setRating(int rating) {
-		if(rating > 1 || rating < 5)
+		if(rating > 1 || rating < 5) //Has to be within 1-5
 			this.rating = rating;
 		else
 			this.rating = 1;// if less than 1 it will set it back to 1
 	}
-	
+	public boolean equals(Movie aMovie) {
+		return aMovie != null &&
+				this.name.equals(aMovie.getName()) &&
+				this.year == aMovie.getYear() &&
+				this.rating == aMovie.getRating() &&
+				this.director.equals(aMovie.getDirector()) &&
+				this.boxOfficeGross == aMovie.getBoxOfficeGross();
+	}
+	public boolean equals(Object o) {//overload for o
+		return this.equals((Movie)o);
+	}
+	public String toString() {
+		return this.name+" " +this.director+" "+this.year+" "+this.rating+" "+
+				this.boxOfficeGross;
+	}
 }
