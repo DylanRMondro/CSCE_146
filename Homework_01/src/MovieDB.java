817b2c9746a571c8bd0b60ef9749611e466b4aa4
BuildTools <unconfigured@null.spigotmc.org>
2019-02-07 22:48:51 -0500

    added readFromFile and removeMovie

--- Homework_01/src/MovieDB.java
+++ Homework_01/src/MovieDB.java
@@ -1,4 +1,30 @@
 //Dylan Mondro
+import java.util.*;
 public class MovieDB {
-
+	private GenLinkedList<Movie> movie;
+	public static final String DELM = "\t";
+	public MovieDB() {
+		movie = new GenLinkedList<Movie>();//WIll create a new instance of movie
+	}
+	public void addMovie(Movie aMovie) {//Will add a movie in the dataBase
+		movie.insert(aMovie);
+	}
+	public void removeMovie(Movie aMovie) {//Will remove a movie from the dataBase
+		movie.gotoItem(aMovie);
+		movie.deleCurrent();
+	}
+	public void printMovies() {
+		//TODO
+	}
+	public void printToFile() {
+		//TODO
+	}
+	public void readFromFile(String fileName) {// will read from a file that we create
+		try {
+			
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+	}
 }
