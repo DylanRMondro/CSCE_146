f8e483a1dc3a03545f53e8da4bd08206ee9fe7f3
BuildTools <unconfigured@null.spigotmc.org>
2019-02-12 23:47:09 -0500

    Finished player

--- HomeWork02/src/Player.java
+++ HomeWork02/src/Player.java
@@ -1,7 +1,30 @@
 //Dylan Mondro
 public class Player {
 	private String name;
+	private Boolean inTheGame;
 	public Player() {
 		this.name = "It's a me";
+		this.inTheGame = true;
+	}
+	public Player(String aName, Boolean aInTheGame) {
+		this.name = aName;
+		this.inTheGame = aInTheGame;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Boolean getInTheGame() {
+		return inTheGame;
+	}
+	public void setInTheGame(Boolean inTheGame) {
+		this.inTheGame = inTheGame;
+	}
+	public boolean stillInGame() { //the check to see if player are in the game
+		if(name !=null && inTheGame ==true)
+			return true;
+		return false;
 	}
 }
