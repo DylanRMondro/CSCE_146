68edd263650c1000689ea4c70bff753ea533a7ad
dylan <dylan@DESKTOP-78RB533>
2019-02-13 15:30:37 -0500

    create

--- HomeWork02/src/ArrayCircQueue.java
+++ HomeWork02/src/ArrayCircQueue.java
@@ -8,5 +8,11 @@ public class ArrayCircQueue {
 	public ArrayCircQueue() {
 		
 	}
-	
+	public void init(int size) {
+		if(size <=1) {
+			return;
+		}
+		tailIndex = headIndex = 0;
+		queue = (P[])(new Object[size]);
+	}
 }
