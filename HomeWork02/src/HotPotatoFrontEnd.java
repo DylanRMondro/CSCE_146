b9c53761515a531b83497c1695ccb78f4c7cf8bd
dylan <dylan@DESKTOP-78RB533>
2019-02-15 12:46:14 -0500

    ...

--- HomeWork02/src/HotPotatoFrontEnd.java
+++ HomeWork02/src/HotPotatoFrontEnd.java
@@ -7,15 +7,17 @@ public class HotPotatoFrontEnd {
 		// TODO Auto-generated method stub
 		Scanner key = new Scanner(System.in);
 		Random ran = new Random(170+10); 
-		Boolean t = true;
+		Boolean t = true;// will automaticaly set the players inGmae to true
 		System.out.println("Welcome to the Hot potato game\n How many players will be in this game?");
-		int aPlayer = key.nextInt();
+		int aPlayer = key.nextInt(); //amount of players in the game;
+		
+		//adding players to the queue
 		ArrayCircQueue ay = new ArrayCircQueue(aPlayer);
 		for(int i=0; i<aPlayer; i++) {
 			System.out.println("Enter the palyer" + i + "name");
 			String aName = key.nextLine();
 			Player py = new Player(aName, t);
-			py = ay.
+			py = ay.enqueue(py.getName());
 		}
 	}
 
