458a3a14eb00ae0a01c1f9685dc4743f835a541a
BuildTools <unconfigured@null.spigotmc.org>
2019-02-21 18:52:47 -0500

    commit

---
+++ Homework03/src/BCReader.java
@@ -0,0 +1,16 @@
+//Dylan Mondro
+public class BCReader {
+	//Will read the board.txt and robotCommands.txt
+	private StringLinkListQueue <String> Queue; //creates the command queue
+	private Board LookABoard = new Board();
+	
+	//read the board
+	public void boardReader(String aFile) {
+		
+	}
+	
+	//read the command
+	public void commandRead(String aFile) {
+		
+	}
+}
